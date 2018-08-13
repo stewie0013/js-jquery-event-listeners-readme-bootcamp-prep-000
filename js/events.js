b72b7dbd794a7d1d9f3fd461f461831a2d37e938
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-13 19:24:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,14 +14,19 @@ function frameIt()
 
 function pressIt()
 {
-$('form').on('keydown', function() {
-  
-})
+  $('form').on('keydown', function(key) {
+    if (key.which === 'G')
+    {
+      alert('You pressed "G".')
+    }
+  })
 }
 
 function submitIt()
 {
-  
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
