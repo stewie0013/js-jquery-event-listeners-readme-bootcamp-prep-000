d7c5586d14535f0daedbbb552c47bb756034995c
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-13 19:25:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt()
 function pressIt()
 {
   $('form').on('keydown', function(key) {
-    if (key.which === 'G')
+    if (key.which === 71)
     {
       alert('You pressed "G".')
     }
